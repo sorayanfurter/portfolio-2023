4ba61cfba8a0b89b9f0b869b9e7bf5f1e3805402
sorayanfurter <sorinfurter@gmail.com>
2023-07-22 04:19:05 -0300

    CSS changes, download PDF button

--- portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
+++ portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
@@ -10,11 +10,14 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
+  downloadPdf(){
+    let link = document.createElement('a');
+    link.download = "CV-SORAYA-FERULANO-PAEZ.pdf";
+    link.href = "assets/CV-SORAYA-FERULANO-PAEZ-2023.pdf";
+    link.click();
+
+
+  }
 }
-
-
-
-
