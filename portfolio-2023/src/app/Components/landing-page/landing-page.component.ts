b4f094cf418c491395a2272d989eaf549c0d0f0e
sorayanfurter <sorinfurter@gmail.com>
2023-12-22 17:27:36 -0300

    fix resume link and hotelwebsite project link

--- portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
+++ portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
@@ -15,7 +15,7 @@ export class LandingPageComponent implements OnInit {
   downloadPdf(){
     let link = document.createElement('a');
     link.download = "CV-SORAYA-FERULANO-PAEZ.pdf";
-    link.href = "assets/CV-SORAYA-FERULANO-PAEZ-2023.pdf";
+    link.href = "assets/CV-SORAYA-FERULANO-PAEZ-2023V2.pdf";
     link.click();
 
 
