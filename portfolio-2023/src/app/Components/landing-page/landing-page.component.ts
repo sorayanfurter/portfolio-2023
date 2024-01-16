a762119a9e2df182bfedb5e75fa4961d5167c5bf
sorayanfurter <sorinfurter@gmail.com>
2024-01-16 16:32:20 -0300

    Fix resume link in landing-page component

--- portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
+++ portfolio-2023/src/app/Components/landing-page/landing-page.component.ts
@@ -14,8 +14,8 @@ export class LandingPageComponent implements OnInit {
 
   downloadPdf(){
     let link = document.createElement('a');
-    link.download = "CV-SORAYA-FERULANO-PAEZ.pdf";
-    link.href = "assets/CV-SORAYA-FERULANO-PAEZ-2023V2.pdf";
+    link.download = "CV-SORAYA-FERULANO-PAEZ-ENG-2024.pdf";
+    link.href = "/assets/CV-SORAYA-FERULANO-PAEZ-ENG-2024.pdf"
     link.click();
 
 
