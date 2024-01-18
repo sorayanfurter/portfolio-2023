f0f1ead5aa34e51e1a95d19884869f7e20fd789f
sorayanfurter <sorinfurter@gmail.com>
2024-01-18 12:44:08 -0300

    Fix navbar animation bug

--- portfolio-2023/src/app/Components/navbar/navbar.component.ts
+++ portfolio-2023/src/app/Components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+
 })
-export class NavbarComponent {
+export class NavbarComponent{
 
   isGlassEffect = false;
 
